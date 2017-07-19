a8355ba1bcaf9a16e216cda08cc26aa6450d3b37
cvelekv <cvelekv@gmail.com>
2017-07-19 22:44:21 +0200

    - Added allow origin header to getDoctors API

--- backend/api/getDoctors.php
+++ backend/api/getDoctors.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *"); //This allows you to send request to same machine (from origin to origin)
 include("../controllers/doctors.php"); // include controller doctors, here we put doctor actions
 $doktori = getDoctors(); // function that returns all doctors
 echo $doktori; // echo doctors to the user
