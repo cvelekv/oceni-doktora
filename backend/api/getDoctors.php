b8783c3fb52f02a517f555e33bf4237665d33558
Nikola Cvetkovic <cvelekv@gmail.com>
2017-08-07 23:45:17 +0200

    no message

--- backend/api/getDoctors.php
+++ backend/api/getDoctors.php
@@ -1,5 +1,6 @@
 <?php
-include("../controllers/doctors.php"); // include controller doctors, here we put doctor actions
+header("Access-Control-Allow-Origin: *"); //This allows you to send request to same machine (from origin to origin)
+include("../controllers/doctorsService.php"); // include controller doctors, here we put doctor actions
 $doktori = getDoctors(); // function that returns all doctors
 echo $doktori; // echo doctors to the user
 ?>
